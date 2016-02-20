37d93286075c177f1d4606d2155d3e8e3a203f86
Arnar Yngvason <arnar1@gmail.com>
2016-02-20 17:03:00 +0000

    updated info

--- setup.py
+++ setup.py
@@ -4,22 +4,26 @@ from setuptools import setup
 from pip.req import parse_requirements
 from pip.download import PipSession
 
+
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 install_reqs = parse_requirements(os.path.join(BASE_PATH, 'requirements.txt'),
                                   session=PipSession())
 
 setup(
     name="simplegexf",
-    version="0.1.0",
+    version="0.1.2",
     description="A simple .gexf parser/writer for python",
     license="MIT",
-    author="Arnar Yngvason",
     url="https://github.com/demux/simplegexf",
-    # packages=['simplegexf'],
     install_requires=[str(ir.req) for ir in install_reqs],
     long_description=open("README.md").read(),
+    keywords="simplegexf gephi gexf",
     classifiers=[
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3 :: Only",
+        "Development Status :: 3 - Alpha",
+        "Topic :: Text Processing :: Markup :: XML",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "License :: OSI Approved :: MIT License",
     ]
 )
